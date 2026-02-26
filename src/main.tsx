c7480f278220e23685d8c9251f4c97dae212e7c2
uozumi <k.uozumi@dynamis.llc>
2026-02-26 15:15:20 +0900

    main.tsx不要なインポート削除

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,5 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
 import PomodoroTimer from './PomodoroTimer.tsx'
 
 createRoot(document.getElementById('root')!).render(
